b19f757b0c7718ad1372d7344bc2a6c0f657375e
taylor <taylor@whatsmyoffer.com>
2022-11-14 20:41:02 -0600

    Like updated

--- resources/js/Components/Like.vue
+++ resources/js/Components/Like.vue
@@ -4,7 +4,7 @@ import {Link, useForm} from '@inertiajs/inertia-vue3';
 
 import {format} from 'friendly-numbers';
 import {reactive} from "vue";
-import {Inertia } from "@inertiajs/inertia";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const props = defineProps(['chirp']);
 
@@ -22,10 +22,10 @@ const toggleLike = function (){
 
 <template>
   <div>
-    <button @click="toggleLike"
-          type="button" :class="(state.liked? 'bg-blue-500' : 'bg-gray-500') +' text-white px-2 rounded-lg'">
-        <i class="fa-duotone fa-thumbs-up me-2"></i> {{format(chirp.likes_count)}} Likes
-    </button>
+    <PrimaryButton @click="toggleLike"
+          :class="(state.liked? 'bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150' : '')">
+      <i class="fa-duotone fa-thumbs-up mr-2"></i> <span>{{format(chirp.likes_count)}} {{chirp.likes_count === 1?'Like':'Likes'}}</span>
+    </Primarybutton>
   </div>
 </template>
 

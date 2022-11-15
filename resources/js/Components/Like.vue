bfed7c380665115bcd5797a642768df79fe4ba06
taylor <taylor@whatsmyoffer.com>
2022-11-14 18:54:43 -0600

    Like button semi fixed in production

--- resources/js/Components/Like.vue
+++ resources/js/Components/Like.vue
@@ -3,19 +3,24 @@
 import { Link } from '@inertiajs/inertia-vue3';
 
 import {format} from 'friendly-numbers';
+import {reactive} from "vue";
 
 const props = defineProps(['chirp']);
 
-const liked = props.chirp.likes.length > 0;
+const state = reactive({liked : props.chirp.likes.length > 0});
+
+const toggleLike = function (){
+  state.liked = !state.liked
+};
 </script>
 
 <template>
   <div>
-    <Link v-if="liked" :href="route('chirps.likes.destroy',[chirp.id, chirp.likes[0].id])" method="delete" as="button" @click="liked = !liked"
+    <Link v-if="state.liked" :href="route('chirps.likes.destroy',[chirp.id, chirp.likes[0].id])" method="delete" as="button" @click="toggleLike"
           type="button" class="bg-blue-500 text-white px-2 rounded-lg" preserve-scroll>
         <i class="fa-duotone fa-thumbs-up me-2"></i> {{format(chirp.likes_count)}} Likes
     </Link>
-    <Link v-else :href="route('chirps.likes.store',chirp.id)" method="post" as="button" type="button" @click="liked = !liked"
+    <Link v-else :href="route('chirps.likes.store',chirp.id)" method="post" as="button" type="button" @click="toggleLike"
           class="bg-gray-500 text-white px-2 rounded-lg" preserve-scroll>
         <i class="fa-duotone fa-thumbs-up me-2"></i> {{format(chirp.likes_count)}} Likes
     </Link>

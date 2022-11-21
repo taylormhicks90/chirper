2d17d44cf5690cf6ef8ce39f74cb397ad9a24e5a
taylor <taylor@whatsmyoffer.com>
2022-11-21 10:20:45 -0600

    Fixed migrations

--- database/migrations/2016_02_07_000000_create_likeable_tables.php
+++ database/migrations/2016_02_07_000000_create_likeable_tables.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -8,18 +9,16 @@ class CreateLikeableTables extends Migration
     public function up()
     {
         Schema::create('likeable_likes', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('likeable_id', 36);
-            $table->string('likeable_type', 255);
-            $table->string('user_id', 36)->index();
+            $table->id();
+            $table->morphs('likeable');
+            $table->foreignIdFor(User::class)->index()->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->unique(['likeable_id', 'likeable_type', 'user_id'], 'likeable_likes_unique');
         });
-        
+
         Schema::create('likeable_like_counters', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('likeable_id', 36);
-            $table->string('likeable_type', 255);
+            $table->id();
+            $table->morphs('likeable');
             $table->unsignedBigInteger('count')->default(0);
             $table->unique(['likeable_id', 'likeable_type'], 'likeable_counts');
         });

6e6863e531d9ebaece9fcf1dec049dc561b0841b
taylor <taylor@whatsmyoffer.com>
2022-11-21 11:52:51 -0600

    Updated User Component

--- resources/js/Components/User.vue
+++ resources/js/Components/User.vue
@@ -3,6 +3,7 @@ import {Link} from "@inertiajs/inertia-vue3";
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import {format} from 'friendly-numbers';
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 dayjs.extend(relativeTime);
 
@@ -11,16 +12,15 @@ const props = defineProps(['user']);
 </script>
 
 <template>
-  <div class="p-6 flex space-x-2">
-    <div class="flex-1">
-      <div class="flex justify-between items-center">
-        <div>
+    <div class="flex p-6">
+      <div class="">
           <Link :href="route('users.show',user.id)">{{user.name }}</Link>
-          <small class="ml-2 text-sm text-gray-600"> Joined {{ dayjs(user.created_at).fromNow() }}</small>
-        </div>
-        <Link :href="route('users.show',user.id)" as="button" type="button" class="bg-indigo-50 px-2 rounded-lg">View</Link>
+          <small class="ml-2 text-sm text-gray-600"> Joined {{ dayjs(user.created_at).fromNow() }}</small><br/>
+          <span>{{format(user.chirps_count)}} Chirps</span>
       </div>
-      <span>{{format(user.chirps_count)}} Chirps</span>
+      <div class="ml-auto my-auto">
+        <Link :href="route('users.show',user.id)" class="" ><PrimaryButton>View</PrimaryButton></Link>
+      </div>
     </div>
-  </div>
+
 </template>
